387b95629a096871fd361c1d40fb0aa0f6f74653
Laurent Cornélis <lcor1979@gmail.com>
2017-01-31 13:05:47 +0100

    Initial commit

---
+++ moisture-monitor-sensor-init-gatt/moisture-monitor-sensor-init-gatt.ino
@@ -0,0 +1,176 @@
+#include <Arduino.h>
+#include <SPI.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+  #include <SoftwareSerial.h>
+#endif
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
+#define VERBOSE_MODE                   true  // If set to 'true' enables debug output
+
+/*=========================================================================
+    APPLICATION SETTINGS
+
+    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
+   
+                              Enabling this will put your Bluefruit LE module
+                              in a 'known good' state and clear any config
+                              data set in previous sketches or projects, so
+                              running this at least once is a good idea.
+   
+                              When deploying your project, however, you will
+                              want to disable factory reset by setting this
+                              value to 0.  If you are making changes to your
+                              Bluefruit LE device via AT commands, and those
+                              changes aren't persisting across resets, this
+                              is the reason why.  Factory reset will erase
+                              the non-volatile memory where config data is
+                              stored, setting it back to factory default
+                              values.
+       
+                              Some sketches that require you to bond to a
+                              central device (HID mouse, keyboard, etc.)
+                              won't work at all with this feature enabled
+                              since the factory reset will clear all of the
+                              bonding data stored on the chip, meaning the
+                              central device won't be able to reconnect.
+    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
+    MODE_LED_BEHAVIOUR        LED activity, valid options are
+                              "DISABLE" or "MODE" or "BLEUART" or
+                              "HWUART"  or "SPI"  or "MANUAL"
+    -----------------------------------------------------------------------*/
+    #define FACTORYRESET_ENABLE         1
+    #define MINIMUM_FIRMWARE_VERSION    "0.7.7"
+    #define MODE_LED_BEHAVIOUR          "SPI"
+/*=========================================================================*/
+
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+// The service and characteristic index information 
+int32_t gattServiceId;
+int32_t gattNewMeasureCharId;
+int32_t gattTemperatureCharId;
+int32_t gattMoistureCharId;
+int32_t gattBatteryCharId;
+
+void setup(void)
+{
+  //remove these 2 lines if not debugging - nothing will start until you open the serial window
+  while (!Serial); // required for Flora & Micro
+  delay(500);
+
+  boolean success;
+
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Custom GATT Service Example"));
+  Serial.println(F("---------------------------------------------------"));
+
+  randomSeed(micros());
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  /* Perform a factory reset to make sure everything is in a known state */
+  Serial.println(F("Performing a factory reset: "));
+  if (! ble.factoryReset() ){
+       error(F("Couldn't factory reset"));
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  /* Add the Custom GATT Service definition */
+  /* Service ID should be 1 */
+  Serial.println(F("Adding the Custom GATT Service definition: "));
+  success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=0x1900"), &gattServiceId);
+  if (! success) {
+    error(F("Could not add Custom GATT service"));
+  }
+  else {
+    Serial.print( F("Gatt Service ID is :") ) ;
+    Serial.println( gattServiceId ) ;
+  }
+
+  Serial.println(F("Adding the NewMeasure characteristic: "));
+  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=1901,PROPERTIES=0x10,MIN_LEN=1, MAX_LEN=1, DESCRIPTION=New measure available, VALUE=0x00"), &gattNewMeasureCharId);
+    if (! success) {
+    error(F("Could not add NewMeasure characteristic"));
+  }
+  else {
+    Serial.print( F("NewMeasure Char ID is :") ) ;
+    Serial.println( gattNewMeasureCharId ) ;
+  }
+  
+  Serial.println(F("Adding the Temperature characteristic: "));
+  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=1902,PROPERTIES=0x02,MIN_LEN=1, MAX_LEN=10, DESCRIPTION=Temperature value, VALUE=0.0"), &gattTemperatureCharId);
+    if (! success) {
+    error(F("Could not add Temperature characteristic"));
+  }
+  else {
+    Serial.print( F("Temperature Char ID is :") ) ;
+    Serial.println( gattTemperatureCharId ) ;
+  }
+
+  Serial.println(F("Adding the Moisture characteristic: "));
+  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=1903,PROPERTIES=0x02,MIN_LEN=1, MAX_LEN=10, DESCRIPTION=Moisture value, VALUE=0.0"), &gattMoistureCharId);
+    if (! success) {
+    error(F("Could not add Moisture characteristic"));
+  }
+  else {
+    Serial.print( F("Moisture Char ID is :") ) ;
+    Serial.println( gattMoistureCharId ) ;
+  }
+
+  Serial.println(F("Adding the Battery characteristic: "));
+  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=1904,PROPERTIES=0x02,MIN_LEN=1, MAX_LEN=10, DESCRIPTION=Battery value, VALUE=0.0"), &gattBatteryCharId);
+    if (! success) {
+    error(F("Could not add Battery characteristic"));
+  }
+  else {
+    Serial.print( F("Battery Char ID is :") ) ;
+    Serial.println( gattBatteryCharId ) ;
+  }
+
+  /* Set device name */
+  Serial.println(F("Setting device name: "));
+  ble.sendCommandCheckOK( F("AT+GAPDEVNAME=MoistureSensor") );
+
+  /* Add the Custom GATT Service to the advertising data */
+  //0x1312 from AT+GATTLIST - 16 bit svc id
+  Serial.println(F("Adding Custom GATT Service UUID to the advertising payload: "));
+  ble.sendCommandCheckOK( F("AT+GAPSETADVDATA=02-01-06-03-02-00-19") );
+
+  /* Reset the device for the new service setting changes to take effect */
+  Serial.println(F("Performing a SW reset (service changes require a reset): "));
+  ble.reset();
+
+  ble.sendCommandCheckOK( F("AT+GATTLIST") );
+
+  Serial.println();
+}
+
+void loop(void)
+{
+
+}
---
+++ moisture-monitor-sensor/moisture-monitor-sensor.ino
@@ -0,0 +1,223 @@
+#include <String.h>
+#include <Adafruit_SleepyDog.h>
+#include <Arduino.h>
+#include <SPI.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+  #include <SoftwareSerial.h>
+#endif
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
+/*=========================================================================
+    APPLICATION SETTINGS
+
+    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
+    MODE_LED_BEHAVIOUR        LED activity, valid options are
+                              "DISABLE" or "MODE" or "BLEUART" or
+                              "HWUART"  or "SPI"  or "MANUAL"
+    -----------------------------------------------------------------------*/
+    #define MINIMUM_FIRMWARE_VERSION    "0.7.7"
+    #define MODE_LED_BEHAVIOUR          "SPI"
+/*=========================================================================*/
+
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+/* GATT ATTR IDS */
+const String NEW_MEASURE_CHAR = "1";
+const String TEMPERATURE_CHAR = "2";
+const String MOISTURE_CHAR = "3";
+const String BATTERY_CHAR = "4";
+
+/* Red led PIN */
+#define LED_PIN 13    
+
+/* Init DHT sensor */
+#include "DHT.h"
+
+#define DHTPIN A0    // DHT22 PIN
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
+
+/* Enable to go in low power mode */
+#define WATCHDOG_ENABLE 1
+
+const unsigned long SLEEP_TIME_WAIT_FOR_CONNECTION = 300000;
+const unsigned long SLEEP_TIME_STAY_UP = 180000;
+const unsigned long SLEEP_TIME_LOW_POWER = 600000;
+
+#define VBATPIN A9   // Battery PIN
+#define BAT_MIN 3.2  // When Battery reach this voltage, it is low
+#define BAT_MAX 3.7  // When Battery reach this voltage, it is full (it can be up to 4.2 but should stay around 3.7)
+
+// A small helper
+void error(String err) {
+  Serial.print(F("Error: "));
+  Serial.println(err);
+}
+
+boolean initBluetooth() {
+    /* Initialise the module */
+  Serial.println(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) ) {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+    return false;
+  }
+  else {
+    Serial.println( F("OK!") );    
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  Serial.println(F("******************************"));
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) ) {
+    // Change Mode LED Activity
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+  }
+
+  Serial.println(F("******************************"));
+
+  return true;
+}
+
+void putDeviceInFailMode() {
+  while(1) {
+    blink(2000, 500);
+  }
+}
+
+unsigned long blink(unsigned long highDelay, unsigned long lowDelay) {
+  digitalWrite(LED_PIN, HIGH);
+  delay(highDelay);
+  digitalWrite(LED_PIN, LOW);
+  delay(lowDelay);  
+
+  return highDelay + lowDelay;
+}
+
+void setup() {
+  while (!Serial);  // required for Flora & Micro
+  delay(500);
+
+  Serial.begin(115200);
+
+  Serial.println(F("Initialising DHT module: "));
+  dht.begin();
+
+  pinMode(LED_PIN, OUTPUT);
+
+  if (!initBluetooth()) {
+    putDeviceInFailMode();
+  }
+
+}
+
+
+float readBatteryVoltage() {
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024; // convert to voltage
+  
+  return measuredvbat;
+}
+
+float getBatteryInPercent() {
+  float batteryVoltage = readBatteryVoltage();
+  
+  return min(((batteryVoltage - BAT_MIN) / (BAT_MAX - BAT_MIN)) * 100.0, 100.0);
+}
+
+String floatToString(float f) {
+  if (isnan(f)) {
+    return F("NaN");
+  }
+  else {
+    return String(f, 2);
+  }
+}
+
+void setGattAttrValue(String attrId, String value) {
+    String gattCommand = "AT+GATTCHAR=" + attrId + ","+ value;
+    ble.println( gattCommand ); 
+    if ( !ble.waitForOK() )
+    {
+      error("Gatt attribute update failed for attrId " + attrId + " and value " + value);
+    }
+    else {
+      Serial.println("Gatt attribute " + attrId + " updated to value " + value);    
+    }
+}
+
+void loop() {
+  if (!ble.isConnected()) {
+    Serial.println(F("Waiting for connection"));
+  }
+  
+  unsigned long timeSpent = 0;
+
+  /* Wait SLEEP_TIME_WAIT_FOR_CONNECTION for connection */
+  while (!ble.isConnected() && timeSpent < SLEEP_TIME_WAIT_FOR_CONNECTION) {
+      timeSpent += blink(500, 500);
+  }  
+
+  if (ble.isConnected()) {
+    digitalWrite(LED_PIN, HIGH);
+    
+    Serial.println(F("Connected"));
+
+    String battery = floatToString(getBatteryInPercent());
+    Serial.println("Battery: " + battery); 
+
+    String humidity = floatToString(dht.readHumidity());
+    Serial.println("Humidity: " + humidity);
+
+    String temperature = floatToString(dht.readTemperature());
+    Serial.println("Temperature: " + temperature);
+
+    setGattAttrValue(NEW_MEASURE_CHAR, "1");
+    setGattAttrValue(BATTERY_CHAR, battery);
+    setGattAttrValue(MOISTURE_CHAR, humidity);
+    setGattAttrValue(TEMPERATURE_CHAR, temperature);
+
+    // Long blink for SLEEP_TIME_STAY_UP to indicate a new measure is ready
+    timeSpent = 0;
+    while (timeSpent < SLEEP_TIME_STAY_UP) {
+      timeSpent += blink(2000, 2000);
+    }  
+  }
+  else {
+    Serial.println(F("No connection"));    
+  }
+
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+
+  if ( WATCHDOG_ENABLE ) {
+    // If enabled, go in sleep mode
+    timeSpent = 0;
+    while (timeSpent < SLEEP_TIME_LOW_POWER) {
+      timeSpent += Watchdog.sleep();
+    }  
+  }
+  else {
+    delay(SLEEP_TIME_LOW_POWER);    
+  }
+}
